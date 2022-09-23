c117aa13643d304f1fb4141c9b1ce465e566b924
Sidney Cardoso <sidneyrsc342@gmail.com>
2022-09-22 22:14:07 -0300

    Login e logout de usuários

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,8 +1,10 @@
 const Login = require('../models/LoginModel')
 
 exports.index = (req, res, next) => {
-    res.render('login')
+    if (req.session.user) return res.render('logged')
+    return res.render('login')
 }
+
 exports.register = async (req, res) => {
     try {
         const login = new Login(req.body)
@@ -19,4 +21,29 @@ exports.register = async (req, res) => {
         console.error(err)
         return res.render('404')
     }
+}
+
+exports.login = async (req, res) => {
+    try {
+        const login = new Login(req.body)
+        await login.login()
+
+        if (login.errors.length > 0) {
+            req.flash('errors', login.errors)
+            req.session.save(() => res.redirect('/login/index'))
+            return
+        }
+
+        req.flash('success', 'Login realizado com sucesso!')
+        req.session.user = login.user
+        req.session.save(() => res.redirect('/login/index'))
+    } catch (err) {
+        console.error(err)
+        return res.render('404')
+    }
+}
+
+exports.logout = (req, res) => {
+    req.session.destroy()
+    res.redirect('/')
 }
